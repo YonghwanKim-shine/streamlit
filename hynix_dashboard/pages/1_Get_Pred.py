3e11e3ffa1e828f3f20ee51ae17c7c62dd07fca2
jiyoung <qkrwldudjyp@gmail.com>
2024-12-23 15:34:27 +0900

    하나만 불러오기

--- hynix_dashboard/pages/1_Get_Pred.py
+++ hynix_dashboard/pages/1_Get_Pred.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 # Google Drive 공유 링크
 url_position_1 = "https://drive.google.com/uc?id=1ajvBW_OH96xtlJZ9aronGZsbtRJkuswp"
-url_position_2 = "https://drive.google.com/uc?id=19YOKChOwa4S6ynO00SKauMPaZtfPuQkC"
-url_position_3 = "https://drive.google.com/uc?id=1Q0S8UAyvX7cHsOAbhcS026y0EFHmByCK"
-url_position_4 = "https://drive.google.com/uc?id=18KindmSQqoQuu4iV7ySE9q4cETC10Lf5"
+# url_position_2 = "https://drive.google.com/uc?id=19YOKChOwa4S6ynO00SKauMPaZtfPuQkC"
+# url_position_3 = "https://drive.google.com/uc?id=1Q0S8UAyvX7cHsOAbhcS026y0EFHmByCK"
+# url_position_4 = "https://drive.google.com/uc?id=18KindmSQqoQuu4iV7ySE9q4cETC10Lf5"
 
 # 파일 다운로드
 
@@ -17,17 +17,17 @@ def download_file(url, output_path):
 @st.cache_data
 def road_file():
     file_path_1 = download_file(url_position_1, "position_1.csv")
-    file_path_2 = download_file(url_position_2, "position_2.csv")
-    file_path_3 = download_file(url_position_3, "position_3.csv")
-    file_path_4 = download_file(url_position_4, "position_4.csv")
+    # file_path_2 = download_file(url_position_2, "position_2.csv")
+    # file_path_3 = download_file(url_position_3, "position_3.csv")
+    # file_path_4 = download_file(url_position_4, "position_4.csv")
 
     df1 = pd.read_csv(file_path_1)
-    df2 = pd.read_csv(file_path_2)
-    df3 = pd.read_csv(file_path_3)
-    df4 = pd.read_csv(file_path_4)
+    # df2 = pd.read_csv(file_path_2)
+    # df3 = pd.read_csv(file_path_3)
+    # df4 = pd.read_csv(file_path_4)
 
-    df = pd.concat([df1,df2,df3,df4], axis=0)
-    return df
+    # df = pd.concat([df1,df2,df3,df4], axis=0)
+    return df1
 
 # 목록 불러오는 기능
 df = road_file()
